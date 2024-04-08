8803d519e00ae657507da2b1cfc1deef2a696196
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-04-08 12:42:32 +0300

    Complted the dom lab by creating and inserting dom nodes

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+// Remove the main
+main.remove()
+
+// new header that point to the h1 node
+ const newHeader = document.createElement("h1")
+
+ //  newHeader has and Id =victory
+
+ newHeader.setAttribute("id", "victory");
+
+// new header with node victory has the text your name is the champion
+
+const yourName = "Lucy";
+
+newHeader.textContent = yourName + "is the champion";
